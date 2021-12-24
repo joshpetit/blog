f7316b956a95a97803f1b83540ae2e6c92995a69
Joshua Petitma <joshua@petit.dev>
2021-12-24 07:27:08 -0500

    Add dates to project page

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,7 +1,7 @@
 import Image from './Image'
 import Link from './Link'
 
-const Card = ({ title, description, imgSrc, href }) => (
+const Card = ({ title, description, imgSrc, href, date = null }) => (
   <div className="p-4 md:w-1/2 md" style={{ maxWidth: '544px' }}>
     <div className="h-full overflow-hidden border-2 border-gray-200 rounded-md border-opacity-60 dark:border-gray-700">
       {href ? (
@@ -33,6 +33,7 @@ const Card = ({ title, description, imgSrc, href }) => (
             title
           )}
         </h2>
+        {date && <h2 className="mb-2 text-2xl font-bold leading-8 tracking-tight">{date}</h2>}
         <p className="mb-3 prose text-gray-500 max-w-none dark:text-gray-400">{description}</p>
         {href && (
           <Link
